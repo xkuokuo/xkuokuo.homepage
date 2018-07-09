42f46455fa347402bcf939e0aa96c6a0473616df
xkuokuo <xinkuo.dev@gmail.com>
2018-07-08 23:35:37 -0700

    Able Send Email To Me

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('#contact-form').submit(function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      data: convertFormToJSON(this),
+      type: $(this).attr('method'),
+      url: $(this).attr('action'),
+      dateaType: 'json',
+      success: function() {
+        alert("谢谢，我会及时回复。");
+      }
+    });
+    return false;
+  });
+});
+
+var convertFormToJSON = function(form) {
+  var arr = $(form).serializeArray();
+  var res = {};
+  arr.forEach((ele) => {res[ele.name] = ele.value})
+  return res;
+}
