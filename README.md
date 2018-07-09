cee54ed18a19ef71969f162e30afdddc1d0aa61b
xkuokuo <xinkuo.dev@gmail.com>
2018-07-08 22:41:30 -0700

    Holly Molly Rick & Morty A Working Serverless API With SAM Cloudformation Setup

---
+++ README.md
@@ -0,0 +1,12 @@
+== 阔阔的个人博客 ==
+Static folder contains all static resources. Just modify.
+
+Stack folder contains AWS cloud formation template.
+```
+# To manually create/update the stack
+sam package --template-file cloudformation.yaml --output-template-file serverless-output.yaml --s3-bucket hello-world-kuoxin
+sam deploy --template-file serverless-output.yaml --stack-name my-home-page --capabilities CAPABILITY_IAM
+
+# To manually the DNS record set in Rout53
+
+```
---
+++ stack/index.js
@@ -0,0 +1,40 @@
+var AWS = require('aws-sdk');
+
+const createResponse = (statusCode, body) => {
+  // to restrict the origin for CORS purposes, replace the wildcard
+  // origin with a specific domain name
+  return {
+    statusCode: statusCode,
+    body: body,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
+};
+
+exports.handler = function(event, context, callback) {
+  // extract message from event
+  var body = JSON.parse(event.body);
+  var email = body.email;
+  var rawMessage = body.message;
+  var name = body.name;
+  var topicARN = process.env.TOPIC_ARN;
+
+  var message = "From: " + name + "\nEmail: " + email + "\n" + rawMessage;
+
+  var sns = new AWS.SNS();
+
+  var snsPublishParams = {
+    Message: message,
+    Subject: "New Visitor Message From Your Homepage!",
+    TopicArn: topicARN
+  }
+
+  sns.publish(snsPublishParams, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log(data);
+  });
+
+  callback(null,createResponse(200, "success"))
+}
